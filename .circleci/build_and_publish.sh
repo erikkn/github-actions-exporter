5b89f019fbc40e61706ac80727af71c659f00587
Nick Platt <nicholas.platt@transferwise.com>
2021-03-25 14:11:41 +0000

    tweaks

--- .circleci/build_and_publish.sh
+++ .circleci/build_and_publish.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
 REGISTRY_URL=docker.tw.ee
-COMMIT=${GITHUB_SHA:0:8}
+COMMIT=${CIRCLIE_SHA1:0:8}
 TAG=v0.1.0-${COMMIT}
 
 echo "Building images"
 
 docker build --pull -t "${REGISTRY_URL}/actions-api-exporter:${TAG}" .
 
-echo "${ARTIFACTORY_PASSWORD}" | docker login ${REGISTRY_URL} --username "${ARTIFACTORY_USER}" --password-stdin
+echo "${REGISTRY_URL}" | docker login ${REGISTRY_URL} --username "${DEPLOY_REGISTRY_USERNAME}" --password-stdin
 
-echo "branch name $GITHUB_REF"
-if [ "$GITHUB_REF" == *"refs/heads/main"* ]
+if [ "$CIRCLE_BRANCH" == "main" ]
 then
   echo "Pushing image.."
   docker push "${REGISTRY_URL}/actions-api-exporter:${TAG}"
